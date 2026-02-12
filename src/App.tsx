18f9ceeaf27949e0047b78b7b728820eec3a0d64
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-12 17:27:50 +0700

    feat: set up initial application structure with routing and global UI providers.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { RouterProvider } from "react-router";
-import { router } from "./router/index";
+import { BrowserRouter } from "react-router";
+import { AppRoutes } from "./router/index";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
-    <TooltipProvider>
-      <ScrollArea className="h-screen w-full">
-        <RouterProvider router={router} />
-      </ScrollArea>
-      
-      <Toaster
-        duration={3000}
-        closeButton
-        position="top-center"
-        theme="system"
-        richColors
-      />
-    </TooltipProvider>
+    <BrowserRouter>
+      <TooltipProvider>
+        <ScrollArea className="h-screen w-full">
+          <AppRoutes />
+        </ScrollArea>
+
+        <Toaster
+          duration={3000}
+          closeButton
+          position="top-center"
+          theme="system"
+          richColors
+        />
+      </TooltipProvider>
+    </BrowserRouter>
   );
 }
 
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,28 +1,19 @@
-import { createBrowserRouter, Navigate } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import LandingPage from "@/pages/public-route-pages/landing-page";
 import AuthPage from "@/pages/public-route-pages/auth-page";
 import UserDashboard from "@/pages/app-route-pages/user-dashboard";
 import AdminDashboard from "@/pages/app-route-pages/admin-dashboard";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/landing-page" replace />,
-  },
-  {
-    path: "/landing-page",
-    element: <LandingPage />,
-  },
-  {
-    path: "/auth-page",
-    element: <AuthPage />,
-  },
-  {
-    path: "/user-dashboard",
-    element: <UserDashboard />,
-  },
-  {
-    path: "/admin-dashboard",
-    element: <AdminDashboard />,
-  },
-]);
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/landing-page" replace />} />
+      <Route path="/landing-page" element={<LandingPage />} />
+      <Route path="/auth-page" element={<AuthPage />} />
+      <Route path="/user-dashboard" element={<UserDashboard />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
+
+      <Route path="*" element={<Navigate to="/landing-page" replace />} />
+    </Routes>
+  );
+}
